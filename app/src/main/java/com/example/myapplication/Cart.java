79bfa87a2ea85cf811c5ee863b3c7e1257b4049c
ASUS <gihanthilakarathna71@gmail.com>
2019-08-06 18:59:34 +0530

    gihan

--- app/src/main/java/com/example/myapplication/Cart.java
+++ app/src/main/java/com/example/myapplication/Cart.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Cart extends AppCompatActivity {
 
