922a4f2386bebb88c87a79c15a3501f07e74685e
ASUS <gihanthilakarathna71@gmail.com>
2019-08-06 19:03:28 +0530

    gihan mama thami

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -1,9 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
 
