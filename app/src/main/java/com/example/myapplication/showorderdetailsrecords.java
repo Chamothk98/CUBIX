36ae26d03d45fe3952902606129958a68f92ff3b
User <yasithrathnayake@gmail.com>
2019-09-16 16:23:33 +0530

    showOrderDetailsRecords

---
+++ app/src/main/java/com/example/myapplication/showorderdetailsrecords.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+public class showorderdetailsrecords {
+
+    private String orderid;
+    private String deliveryaddress;
+    private String totalprice;
+    private String vehicleno;
+    private String starttime;
+    private String endtime;
+
+    public showorderdetailsrecords() {
+    }
+
+    public String getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(String orderid) {
+        this.orderid = orderid;
+    }
+
+    public String getDeliveryaddress() {
+        return deliveryaddress;
+    }
+
+    public void setDeliveryaddress(String deliveryaddress) {
+        this.deliveryaddress = deliveryaddress;
+    }
+
+    public String getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(String totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public String getVehicleno() {
+        return vehicleno;
+    }
+
+    public void setVehicleno(String vehicleno) {
+        this.vehicleno = vehicleno;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+}
