42be5bc6e1dc4547a3e0407acf2136f0bf3c1026
User <yasithrathnayake@gmail.com>
2019-09-16 16:19:37 +0530

    ContactUsRecords

---
+++ app/src/main/java/com/example/myapplication/contactusrecords.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+public class contactusrecords {
+
+    private String name;
+    private String email;
+    private String phoneNo;
+    private String website;
+    private String message;
+
+    public contactusrecords() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
