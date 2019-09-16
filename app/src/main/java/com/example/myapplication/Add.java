6f09c48a6ec9c0fd39f33cdc27c6d920472b3037
Chamoth <chamothk98@gmail.com>
2019-09-17 00:02:30 +0530

    chamoth

--- app/src/main/java/com/example/myapplication/Add.java
+++ app/src/main/java/com/example/myapplication/Add.java
@@ -1,17 +1,96 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Add extends AppCompatActivity {
+
+    EditText Name,Price,Content;
+    Button Add;
+   DatabaseReference dbref;
+   itemadd itm;
+
+
+   private void clearControlls(){
+
+       Name.setText("");
+       Price.setText("");
+       Content.setText("");
+   }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        Name = findViewById(R.id.Name);
+        Price = findViewById(R.id.Price);
+        Content = findViewById(R.id.Content);
+
+        Add = findViewById(R.id.Add);
+        itm = new itemadd();
+
+
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dbref = FirebaseDatabase.getInstance().getReference().child("OrderRecordsAdd");
+
+
+                try {
+                    if (TextUtils.isEmpty(Name.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please Enter", Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(Price.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please Enter", Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(Content.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Please Enter", Toast.LENGTH_SHORT).show();
+
+
+                    else {///take inputs
+                        itm.setName(Name.getText().toString().trim());
+                        itm.setPrice(Price.getText().toString().trim());
+                        itm.setContent(Content.getText().toString().trim());
+
+                        ///insert to database
+                        dbref.push().setValue(itm);
+                        dbref.child("itm1").setValue(itm);
+
+                        ///Feedback
+                        Toast.makeText(getApplicationContext(), "Data added Successfully", Toast.LENGTH_LONG).show();
+                        clearControlls();
+
+
+                    }
+                }
+
+                catch (NumberFormatException e)
+                {
+                    Toast.makeText(getApplicationContext(),"Invalid Details",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
+
     }
+
 
     public void add(View view){
         Intent Ad = new Intent(this,Add.class);
---
+++ app/src/main/java/com/example/myapplication/itemadd.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+public class itemadd {
+
+
+        private String Name;
+        private String Price;
+        private String Content;
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public itemadd(){
+
+
+
+        }
+}
